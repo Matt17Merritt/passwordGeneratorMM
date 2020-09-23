f6b5995e7a2783d6b3d6deaefc19f6f28fbd0d25
Matt Merritt <69483074+Matt17Merritt@users.noreply.github.com>
2020-09-22 22:02:50 -0400

    Added a function to find random lowercase letters to script.js.

--- script.js
+++ script.js
@@ -7,8 +7,17 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Function that pulls random whole numbers for the corresponding lower case letters from character chart.
+function getRandomLowerCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+console.log(getRandomLowerCase());
+
+//console.log(Math.floor(Math.random() * 26) + 97);
+
