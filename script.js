a5899300ca6cde802cf772f1cca69590aac27267
Matt Merritt <69483074+Matt17Merritt@users.noreply.github.com>
2020-09-23 03:58:44 -0400

    Had to backtrack as my methods were conflicting.

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-// Object containing all of the randomizer functions.
-const randomFunc = {
-  lower: getRandomLowerCase,
-  upper: getRandomUpperCase,
-  number: getRandomNumber,
-  symbol: getRandomSymbol
-};
+
 
 // Assigns button ID 'generate' to the variable 'generateBtn'.
 var generateBtn = document.querySelector("#generate");
@@ -20,15 +14,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', function initialPrompts (event) {
-  let characterCount = prompt('How many characters would you like in your password?');
-  let characterUpper =prompt('Would you like uppercase letters? Yes or No');
-  let characterLower =prompt('Would you like lowercase letters? Yes or No');
-  let characterNum =prompt('Would you like numbers? Yes or No');
-  let characterSym =prompt('Would you like symbols? Yes or No');
+  let characterCount = prompt('How many characters would you like in your password? Enter a number from 8 to 128.');
+  console.log(characterCount)
+  let characterUseUpper =confirm('Would you like uppercase letters? Yes or No');
+  console.log(characterUseUpper)
+  let characterUseLower =confirm('Would you like lowercase letters? Yes or No');
+  console.log(characterUseLower)
+  let characterUseNum =confirm('Would you like numbers? Yes or No');
+  console.log(characterUseNum)
+  let characterUseSym =confirm('Would you like symbols? Yes or No');
+  console.log(characterUseSym)
 });
 
 
-  
 
 
 
@@ -45,31 +43,4 @@ generateBtn.addEventListener('click', function initialPrompts (event) {
 
 
 
-// Function that pulls random whole numbers for the corresponding lowercase letters from character chart.
-function getRandomLowerCase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-// Test
-console.log(getRandomLowerCase());
 
-// Function that pulls random whole numbers for the corresponding uppercase letters from character chart.
-function getRandomUpperCase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-// Test
-console.log(getRandomUpperCase());
-
-// Function that pulls random whole numbers for the corresponding numbers from the character chart.
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-// Test
-console.log(getRandomNumber());
-
-// Function that uses the symbol array to randomize what symbol gets picked.
-function getRandomSymbol() {
-  var symbols = '!@#$%^&*(){}[]=<>/,.';
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-// Test
-console.log(getRandomSymbol());
