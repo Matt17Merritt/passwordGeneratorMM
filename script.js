2971de7a87617407e9d811ee5060e10e7918d658
Matt Merritt <69483074+Matt17Merritt@users.noreply.github.com>
2020-09-22 22:29:54 -0400

    Added a function to find random numbers to script.js.

--- script.js
+++ script.js
@@ -12,18 +12,26 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Function that pulls random whole numbers for the corresponding lower case letters from character chart.
+// Function that pulls random whole numbers for the corresponding lowercase letters from character chart.
 function getRandomLowerCase() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-
+// Test
 console.log(getRandomLowerCase());
 
 //console.log(Math.floor(Math.random() * 26) + 97);
 
-// Function that pulls random whole numbers for the corresponding upper case letters from character chart.
+// Function that pulls random whole numbers for the corresponding uppercase letters from character chart.
 function getRandomUpperCase() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-
+// Test
 console.log(getRandomUpperCase());
+
+// Function that pulls random whole numbers for the corresponding numbers from the character chart.
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+// Test
+console.log(getRandomNumber());
+
