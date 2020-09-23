d84c5d5a78ff64c866385f258ebec7d0f70e3f68
Matt Merritt <69483074+Matt17Merritt@users.noreply.github.com>
2020-09-23 03:17:31 -0400

    Added initial prompts when generator button is clicked.

--- script.js
+++ script.js
@@ -9,37 +9,27 @@ const randomFunc = {
 // Assigns button ID 'generate' to the variable 'generateBtn'.
 var generateBtn = document.querySelector("#generate");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Write password to the #password input. This is the generate password function.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
   passwordText.value = password;
-
-
-
-
-
-
+  
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener ('click', writePassword()) {
-  alert('password stuff');
-}
+generateBtn.addEventListener('click', function initialPrompts (event) {
+  let characterCount = prompt('How many characters would you like in your password?');
+  let characterUpper =prompt('Would you like uppercase letters? Yes or No');
+  let characterLower =prompt('Would you like lowercase letters? Yes or No');
+  let characterNum =prompt('Would you like numbers? Yes or No');
+  let characterSym =prompt('Would you like symbols? Yes or No');
+});
+
+
+  
+
 
 
 
