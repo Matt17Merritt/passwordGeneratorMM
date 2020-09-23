f41cc11874f7f766ff41b03de3047b792d6398df
Matt Merritt <69483074+Matt17Merritt@users.noreply.github.com>
2020-09-23 04:34:44 -0400

    added a while loop to check password parameters.

--- script.js
+++ script.js
@@ -18,18 +18,32 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', function initialPrompts (event) {
+generateBtn.addEventListener('click', function writePassword (event) {
   let characterCount = prompt('How many characters would you like in your password? Enter a number from 8 to 128.');
-  console.log(characterCount)
-  let characterUseUpper =confirm('Would you like uppercase letters? Yes or No');
-  console.log(characterUseUpper)
-  let characterUseLower =confirm('Would you like lowercase letters? Yes or No');
-  console.log(characterUseLower)
-  let characterUseNum =confirm('Would you like numbers? Yes or No');
-  console.log(characterUseNum)
-  let characterUseSym =confirm('Would you like symbols? Yes or No');
-  console.log(characterUseSym)
-});
+  console.log(characterCount);
+  let characterUseUpper = confirm('Would you like uppercase letters?');
+  console.log(characterUseUpper);
+  let characterUseLower = confirm('Would you like lowercase letters?');
+  console.log(characterUseLower);
+  let characterUseNum = confirm('Would you like numbers?');
+  console.log(characterUseNum);
+  let characterUseSym = confirm('Would you like symbols?');
+  console.log(characterUseSym);
+  
+    // while loop that checks if the user entered any of the char sets.
+    while (characterUseUpper === false && characterUseLower === false && characterUseNum === false && characterUseSym === false) {
+    alert("Please select one of the following options.");
+    characterUseUpper = confirm('Would you like uppercase letters?');
+    characterUseLower = confirm('Would you like lowercase letters?');
+    characterUseNum = confirm('Would you like numbers?');
+    characterUseSym = confirm('Would you like symbols?');
+
+}});
+
+
+
+
+
 
 
 
